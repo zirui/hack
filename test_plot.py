56fad592b3d52bdccf90ce5858502b0387d2cb61
zirui <zirui.dream@gmail.com>
2015-07-26 00:14:22 +0800

    add test sript for matplotlib

---
+++ test_plot.py
@@ -0,0 +1,12 @@
+
+%matplotlib inline
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = np.linespace(0, 2*np.pi, 500)
+x = 2 * x
+plot.plot(x, np.sin(x ** 2))
+plt.title('haha')
+plt.show()
